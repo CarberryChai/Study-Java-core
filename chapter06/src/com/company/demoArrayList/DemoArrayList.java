441b7fd8c195deeb30543b53f04716d87667c3f4
CarberryChai <CarberryChai@gmail.com>
2019-12-17 18:30:14 +0800

    learn ArrayList

---
+++ chapter06/src/com/company/demoArrayList/DemoArrayList.java
@@ -0,0 +1,27 @@
+package com.company.demoArrayList;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/** 给定20个随机数，返回一个为偶数的ArrayList */
+public class DemoArrayList {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<Integer> filterList = new ArrayList<>();
+    Random random = new Random();
+    for (int i = 0; i < 20; i++) {
+      int num = random.nextInt(100) + 1; // [1, 100]内的随机数
+      list.add(num);
+    }
+    list.forEach(
+        n -> {
+          if (isOdd(n)) filterList.add(n);
+        });
+    System.out.println(filterList.size());
+    System.out.println(filterList);
+  }
+  // 判断一个整数是偶数
+  public static boolean isOdd(int num) {
+    return num % 2 == 0;
+  }
+}
